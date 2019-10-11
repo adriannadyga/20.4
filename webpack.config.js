e934ad7550faee60b863e6a77d0deb0012eafab5
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-10-11 20:26:05 +0200

    Add HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
     const environment = env || 'production';
@@ -28,35 +29,11 @@ module.exports = (env) => {
                     ]
                 }
             ]
-        }
+        },
+        plugins: [new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })]
     }
 }
-
-// module.exports = {
-//     mode: 'development',
-//     entry: './src/index.js',
-//     output: {
-//         path: path.resolve(__dirname, 'build'),
-//         filename: 'app.bundle.js'
-//     },
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.js$/,
-//                 loader: "babel-loader"
-//             },
-//             {
-//                 test: /\.css$/,
-//                 use: [
-//                     {loader: 'style-loader'},
-//                     {
-//                         loader: 'css-loader',
-//                         options: {
-//                             modules: true
-//                         }
-//                     }
-//                 ]
-//             }
-//         ]
-//     }
-// };
