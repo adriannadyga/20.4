b7a8325e2105e489163f17eadbb7561096e627df
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-10-13 11:43:31 +0200

    Add Hot Module Replacement

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import style from './App.css';
 import Title from '../components/Title.js';
 import TodoList from '../components/TodoList.js';
+import { hot } from 'react-hot-loader';
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -48,4 +50,4 @@ class App extends React.Component {
     }
 }
 
-export default App;
+export default hot(module)(App);
