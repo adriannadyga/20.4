2b35ba100dbd84009971a2ae99caa4b76b251ea1
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-10-13 17:21:15 +0200

    Add style to TodoList, change change component to arrow function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import style from './TodoList.css';
 
-class Todolist extends React.Component {
-    constructor(props) {
-        super(props);
-    };
-    render() {
-        const Todolist = this.props.data.map(item => 
-            <li id={item.id} key={item.id}>{item.text}
-            <button onClick={()=> this.props.delete(item.id)}>x</button>
-            </li>);
+// class TodoList extends React.Component {
+//     constructor(props) {
+//         super(props);
+//     };
+//     render() {
+//         const Todolist = this.props.data.map(item => 
+//             <li className={style.onList} id={item.id} key={item.id}>{item.text}
+//             <button className={style.button} onClick={()=> this.props.delete(item.id)}>x</button>
+//             </li>);
         
-        return (
-            <div>
-                <ul>{Todolist}</ul>
-            </div>
-        )
-    }
+//         return (
+//             <div>
+//                 <ul>{Todolist}</ul>
+//             </div>
+//         )
+//     }
+// }
+
+const TodoList = (props) => {
+    return (
+        <ul>
+            {props.data.map(item => {
+                return <li className={style.onList} key={item.id}>{item.text}
+                <button className={style.button} onClick={()=> props.delete(item.id)}>x</button>
+                </li>
+            })}
+        </ul>
+    );
 }
 
-export default Todolist;
+export default TodoList;
